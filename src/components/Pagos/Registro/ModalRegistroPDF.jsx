860e7aacad06425ca666d81555b4237131c2ffcb
JMCA2805 <126287724+JMCA2805@users.noreply.github.com>
2023-08-07 09:19:24 -0400

    Añadiendo Boton PDF

--- src/components/Pagos/Registro/ModalRegistroPDF.jsx
+++ src/components/Pagos/Registro/ModalRegistroPDF.jsx
@@ -1,16 +1,24 @@
 import { useState } from "react";
 import { FaTimes } from "react-icons/fa";
+import { CrearPdfEmpleado } from "../../../pdf/controllers/PdfEmpleado";
 import { PDFDownloadLink, pdf } from "@react-pdf/renderer";
+import { MyDoc } from "../../../pdf/plantillas/Recibo";
 let data = { datos: "datos" };
-let tittle = "Inf_Pag";
+let Pdf = MyDoc();
+let tittle = "Inf_Emp";
 
-export default function ModalPDF(idEmpleados) {
+export default function ModalPDF(dataPago) {
   const [openModal, setOpenModal] = useState(false);
   return (
     <div>
       <button
         className="text-sm border-2 border-black rounded-lg p-1"
         onClick={async () => {
+          data = await CrearPdfEmpleado({idEmpleados:dataPago.dataPago.idEmpleado});
+          Pdf = await MyDoc(data,dataPago);
+          if (data.apellidos != "" && data.apellidos !== undefined) {
+            tittle = data.apellidos;
+          }
           setOpenModal(!openModal);
         }}
       >
@@ -30,7 +38,13 @@ export default function ModalPDF(idEmpleados) {
             onClick={() => setOpenModal(false)}
           />
           <h2 className="text-white font-semibold">Seleccione el pdf</h2>
-          
+          <PDFDownloadLink
+            document={Pdf}
+            fileName={`${tittle}.pdf`}
+            className="bg-white bg-opacity-90 border-2 border-black font-bold w-1/2 p-2 rounded text-center"
+          >
+            Datos personales
+          </PDFDownloadLink>
         </div>
       </div>
     </div>
